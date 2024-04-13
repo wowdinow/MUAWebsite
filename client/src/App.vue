32b4765aa983a699a7787ddc9535d973f4c6a84f
Frando Wadino <f.wadino@gmail.com>
2024-04-13 17:06:11 +0700

    First setup and home setup

---
+++ client/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Home from './views/Home.vue'
+import Details from './views/Details.vue'
+</script>
+
+<template>
+    <RouterView/>
+</template>
+
+<style>
+</style>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,17 @@
+<script>
+import { RouterLink } from 'vue-router'
+// import Home from '../components/Home.vue'
+</script>
+
+<template>
+  <div style="background-color: darkmagenta;" class="vh-100 d-flex flex-column justify-content-center">
+    <div class="mx-auto">
+    <div class="row text-center">
+      <h1>Welcome to Marsellas Make Up</h1>
+    </div>
+    <div class="d-flex justify-content-center">
+      <RouterLink class="btn btn-primary" to="/details">Get Excited</RouterLink>
+    </div>
+  </div>
+  </div>
+</template>
