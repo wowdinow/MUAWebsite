7d11eb07c93b8c563a0e5ac934ca1e9cf24161fd
Frando Wadino <f.wadino@gmail.com>
2024-04-16 17:03:41 +0700

    Updates on home

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,12 +1,35 @@
 <script>
 import { RouterLink } from 'vue-router'
 // import Home from '../components/Home.vue'
+
+
+export default {
+data(){
+  return{
+    items: ['Apple', 'Banana', 'Orange']
+  }
+},
+
+methods: {
+  incrementIndex(key){
+    return key+1
+  }
+}
+}
+
 </script>
 
 <template>
   <div style="background-color: darkmagenta;" class="vh-100 d-flex flex-column justify-content-center">
     <div class="mx-auto">
     <div class="row text-center">
+      <div>
+    <!-- <ul>
+      <li v-for="item in items" :key="item.id">{{ item.name }}</li>
+    </ul> -->
+  </div>
+      <h1 v-for="(item, index) of items" :key="key" class="animate__animated animate__flipInX animate-duration:2s">
+        {{ incrementIndex(index) }}</h1>
       <h1>Welcome to Marsellas Make Up</h1>
     </div>
     <div class="d-flex justify-content-center">
